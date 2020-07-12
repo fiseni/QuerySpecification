38a044149333c7c6b69aee5ec3625349f82c2bae
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-07-12 17:12:42 +0200

    Adding abstract specification and builder with basic functionalities.

---
+++ src/PozitronDev.QuerySpecification/ISpecification.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PozitronDev.QuerySpecification
+{
+    public interface ISpecification<T>
+    {
+        IEnumerable<Expression<Func<T, bool>>> WhereExpressions { get; }
+
+        int Take { get; }
+        int Skip { get; }
+        bool IsPagingEnabled { get; }
+    }
+}
