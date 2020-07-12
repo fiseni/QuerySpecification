142d0eb4fb34a9e41bf10ce0a3f450f49a960969
Fati Iseni <fati.iseni@pozitrongroup.com>
2020-07-12 18:06:31 +0200

    Adding non-EF specification evaluator.

---
+++ src/PozitronDev.QuerySpecification/SpecificationEvaluator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PozitronDev.QuerySpecification
+{
+    public class SpecificationEvaluator<T> : BaseSpecificationEvaluator<T>, ISpecificationEvaluator<T> where T : class
+    {
+        public IQueryable<TResult> GetQuery<TResult>(IQueryable<T> inputQuery, ISpecification<T, TResult> specification)
+        {
+            var query = base.GetQuery(inputQuery, specification);
+
+            // Apply selector
+            var selectQuery = query.Select(specification.Selector);
+
+            return selectQuery;
+        }
+    }
+}
