8c13d494855cc3994eb7c8357498026a8619da92
Fati Iseni <fati.iseni@pozitrongroup.com>
2025-06-07 23:29:34 +0200

    Updated XML comments.

--- src/QuerySpecification/DiscoveryAttribute.cs
+++ src/QuerySpecification/DiscoveryAttribute.cs
@@ -9,31 +9,31 @@ public sealed class SpecAutoDiscoveryAttribute : Attribute
 }
 
 /// <summary>
-/// Specifies discovery options for evaluators and validators, such as order and whether discovery is enabled.
+/// Specifies discovery options for evaluators and validators, such as the order and whether discovery is enabled.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
 public class DiscoveryAttribute : Attribute
 {
     /// <summary>
-    /// Gets the order in which the evaluator should be applied. Lower values are applied first.
+    /// Gets the order in which the evaluator/validator should be applied. Lower values are applied first.
     /// </summary>
     public int Order { get; set; } = int.MaxValue;
 
     /// <summary>
-    /// Gets a value indicating whether the evaluator is discoverable.
+    /// Gets a value indicating whether the evaluator/validator is discoverable.
     /// </summary>
     public bool Enable { get; set; } = true;
 }
 
 /// <summary>
-/// Specifies discovery options for evaluators, such as order and whether discovery is enabled.
+/// Specifies discovery options for evaluators, such as the order and whether discovery is enabled.
 /// </summary>
 public sealed class EvaluatorDiscoveryAttribute : DiscoveryAttribute
 {
 }
 
 /// <summary>
-/// Specifies discovery options for validators, such as order and whether discovery is enabled.
+/// Specifies discovery options for validators, such as the order and whether discovery is enabled.
 /// </summary>
 public sealed class ValidatorDiscoveryAttribute : DiscoveryAttribute
 {
